480e842a4f81f18c014bb9042e5fb0405860a23e
Joseph Thuku <jthuku490@gmail.com>
2023-04-06 19:57:51 +0000

    script to deploy webstatc

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+"""
+script to upload web static
+"""
 from fabric.api import env, put, run, sudo
 import os
 
-#script to deploy webstatic on two servers
 env.hosts = ['18.234.168.198', '35.168.2.70']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
-
 
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
@@ -18,14 +18,13 @@ def do_deploy(archive_path):
         
         archive_filename = os.path.basename(archive_path)
         archive_foldername = archive_filename.split('.')[0]
-        sudo('mkdir -p /data/web_static/releases/{}'.format(archive_foldername))
-        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_foldername))
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_foldername))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_foldername))
         
-        sudo('sudo rm -f /tmp/{}'.format(archive_filename))
+        run('rm /tmp/{}'.format(archive_filename))
+        run('sudo rm -f /data/web_static/current')
         
-        sudo('sudo rm -f /data/web_static/current')
-        
-        sudo('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_foldername))
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_foldername))
 
         print("New version deployed!")
         return True
