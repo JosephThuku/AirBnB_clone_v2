00607a67a336790518edf0f8865496c47d57e0b5
JosephThuku <jthuku490@gmail.com>
2023-04-22 09:07:50 -0700

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """
-Script to start a flask web aplication
+Script that starts a Flask web application:
 """
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route('/')
+def hello_route():
+    """ Display text """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/hbnb')
+def display_hbnb():
+    """ Display text """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route('/c/<text>')
+def display_ctext(text):
+    """ Display C text """
+    return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python')
-@app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    return 'python {}'.format(text.replace('_', ' '))
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """ Display Python text with a default of 'is cool' """
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
